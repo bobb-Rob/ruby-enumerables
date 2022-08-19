4728b714d466bc0c971a0cde011c5bb1722c6f5b
Robertson Arthur <robertsonakpan@gmail.com>
2022-08-19 14:33:07 +0100

    Link module using require_relative

---
+++ MyEnumerable.rb
@@ -0,0 +1,20 @@
+
+module MyEnumerable
+  def get_list(list)
+    list
+  end
+
+  def all?
+    if block_given?
+      yield 478
+  end
+
+  def any? 
+    
+  end
+
+  def filter 
+
+  end
+
+end
--- MyList.rb
+++ MyList.rb
@@ -1,17 +1,20 @@
+require_relative './MyEnumerable.rb'
 
 class MyList 
   def initialize(*args)
     @list = args
   end
 
+  include MyEnumerable
+
   def each
     yield @list
   end
 end
 
-
-# %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
-
 ourList = MyList.new(1,2,3,4,5,6,77,8)
-p ourList
+
 puts ourList.each { |x| puts x }
+
+ourList.all? { |a| p a }
+
